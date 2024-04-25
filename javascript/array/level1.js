aa68745adda9fffbacdf83de22bf26f455fc01c5
Arun <ac.arunchay.ac@gmail.com>
2024-04-25 11:56:32 +0545

    assigment problem

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -4,14 +4,17 @@ const arr = [3,5,2]
 //expected output is 3
 console.log(arr.length)
 
-
-
-
 //Q2 reverse the array and console log
 //expected output [2,5,3]
 const reversed = arr.reverse()
 console.log(reversed)
-///Q3 join the array and console log
+
+//Q3 join the array and console log
 // expected output 352
 console.log(arr.join(''))
 
+//Q4 Find the index of a specific element in the array. Log the index to the console. 
+//expected output: 3
+const allNum = [3, 5, 2, 8, 5];
+const searchElement = 2;
+console.log(allNum.indexOf(searchElement))
--- javascript/array/level2.js
+++ javascript/array/level2.js
@@ -2,3 +2,24 @@ const arr =  [3,5,2]
 //console.log the maximum number of an array
 //expectedd output 5
 console.log(Math.max(...arr));
+
+//Q2 Remove a specific element 2 from the array. Log the modified array to the console.
+// expected output: [3,5]
+const afterElementRemove = arr.slice(0,2)
+console.log(afterElementRemove)
+
+//Q3 Multiply each element of the array by a constant value 5 and log the modified array to the console.
+//expected output [15,10]
+const arr3 = arr.filter(num => num != 5)
+const multiply = arr3.map(afterElementRemove =>afterElementRemove*5)
+console.log(multiply)
+
+//find intersection (common elements) of arr and arr2
+//expected output: [5]
+const arr2 = [5,21,32]
+function performIntersection(arr1, arr2) {
+    const intersectionResult = arr1.filter(x => arr2.indexOf(x) !== -1);
+    return intersectionResult;
+}
+const result = performIntersection(arr, arr2);
+console.log(result);
