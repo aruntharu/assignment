4158ee88841800deadc03334df8d56bc5492c3d4
Arun <ac.arunchay.ac@gmail.com>
2024-04-25 12:06:14 +0545

    assigment solve

--- javascript/array/level1.js
+++ javascript/array/level1.js
@@ -1,5 +1,5 @@
+const arr =  [3,5,2]
 
-const arr = [3,5,2]
 //Q1 calculate length of array and console log
 //expected output is 3
 console.log(arr.length)
@@ -9,12 +9,27 @@ console.log(arr.length)
 const reversed = arr.reverse()
 console.log(reversed)
 
-//Q3 join the array and console log
-// expected output 352
+///Q3 join the array and console log
+// expected output 352  or 253
 console.log(arr.join(''))
 
-//Q4 Find the index of a specific element in the array. Log the index to the console. 
+//Q4 Remove the last element from the array and log the modified array to the console
+
+
+// Q5 Check if all elements in the num  are even. Log "All even" or "Not all even" accordingly.
+const num = [6,4,2,8,10,12]
+const num = [6,4,2,8,10,12]
+let result = "All even"
+num.forEach((item)=>{
+  if (item%2 !== 0){
+    result = "Not all even"
+  }
+})
+console.log(result)
+
+// Find the index of a specific element in the array. Log the index to the console. 
 //expected output: 3
+
 const allNum = [3, 5, 2, 8, 5];
 const searchElement = 2;
 console.log(allNum.indexOf(searchElement))
